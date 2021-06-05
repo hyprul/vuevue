f3bfecc1a0ceab0c8ace93791a7ed745ed8cdeb4
Sean Huang <seanh@ritani.com>
2021-06-05 19:35:27 -0400

    stuff

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,13 +1,13 @@
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const listEl = document.querySelector("ul");
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItem = document.createElement("li");
-  listItem.textContent = enteredValue;
-  listEl.appendChild(listItem);
-  inputEl.value = "";
-}
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItem = document.createElement("li");
+//   listItem.textContent = enteredValue;
+//   listEl.appendChild(listItem);
+//   inputEl.value = "";
+// }
 
-buttonEl.addEventListener("click", addGoal);
+// buttonEl.addEventListener("click", addGoal);
