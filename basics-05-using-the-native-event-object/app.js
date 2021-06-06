7f956cf0de37c4b575608c5b10846e93fdbddeed
Sean Huang <seanh@ritani.com>
2021-06-06 19:12:07 -0400

    bind

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -2,12 +2,12 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: "",
     };
   },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    setName(event) {
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,8 +15,11 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
-    }
-  }
+    },
+    resetInput() {
+      (this.name = ""), (this.counter = 0);
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
