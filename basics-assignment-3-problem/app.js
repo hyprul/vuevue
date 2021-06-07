d28db359fa9d88017a7ac8df619607fad770a4c8
Sean Huang <seanh@ritani.com>
2021-06-06 20:18:29 -0400

    methods, watch, computed

--- basics-assignment-3-problem/app.js
+++ basics-assignment-3-problem/app.js
@@ -1,9 +1,40 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      number: 0,
+    };
   },
-  methods: {},
-  computed: {},
+  methods: {
+    addOne() {
+      this.number += 1;
+    },
+    addFive() {
+      this.number += 5;
+    },
+  },
+  watch: {
+    result() {
+    console.log("Watcher Mounted!")
+      that = this;
+      setTimeout(function () {
+        console.log("hit timer");
+        that.number = 0;
+      }, 5000);
+    },
+  },
+  computed: {
+    result() {
+      if (this.number < 37) {
+        return "Not there yet";
+      }
+      if (this.number == 37) {
+        return "37";
+      }
+      if (this.number > 37) {
+        return "Too Much!";
+      }
+    },
+  },
 });
 
 app.mount("#assignment");
