dd392f580b284a2f5e82cacd3468a3bfa7345e84
Sean Huang <seanh@ritani.com>
2021-06-20 13:46:03 -0400

    long message

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -1,8 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      currentUserInput: '',
-      message: 'Vue is great!',
+      currentUserInput: "",
+      message: "Vue is great!",
     };
   },
   methods: {
@@ -15,4 +15,26 @@ const app = Vue.createApp({
   },
 });
 
-app.mount('#app');
+app.mount("#app");
+
+//
+
+const data = {
+  message: "Hello!",
+  longMessage: "Hello! World!",
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === "message") {
+      target.longMessage = value + "World!";
+    }
+    target.message = value;
+  },
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = "Hello!!!!";
+
+console.log(proxy.longMessage);
