6ab9a666fba4a3e71cd53bfb67b2bac20514f322
Sean Huang <seanh@ritani.com>
2021-06-20 11:46:46 -0400

    attack health

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -1,11 +1,19 @@
 const app = Vue.createApp({
   data() {
     return {
-        monsterHealth: 100,
-        playerHealth: 100
+      monsterHealth: 100,
+      playerHealth: 100,
     };
   },
-  methods: {},
+  methods: {
+    attackMonster() {
+      const attackValue = Math.floor(Math.random() * (12 - 5)) + 5;
+      this.monsterHealth -= attackValue;
+
+      const monsterAttackValue = Math.floor(Math.random() * (12 - 5)) + 5;
+      this.playerHealth -= monsterAttackValue;
+    },
+  },
   computed: {},
 });
 
