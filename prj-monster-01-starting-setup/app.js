c4f38a9bb4e45e13ad2031585335f7f1132fdfb9
Sean Huang <seanh@ritani.com>
2021-06-20 11:42:48 -0400

    vue setup

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -1,1 +1,12 @@
-const app = Vue.createApp({});
+const app = Vue.createApp({
+  data() {
+    return {
+        monsterHealth: 100,
+        playerHealth: 100
+    };
+  },
+  methods: {},
+  computed: {},
+});
+
+app.mount("#game");
