597fef2bd05ebc8936e9c484da3ab4aa490e2505
Sean Huang <seanh@ritani.com>
2021-07-11 14:54:40 -0400

    Styles

--- vue-cli-01-a-new-vue-project/src/main.js
+++ vue-cli-01-a-new-vue-project/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import Contact from "./components/Contact.vue";
+import Form from "./components/Form.vue";
 
 const app = createApp(App);
 app.component("Contact", Contact);
+app.component("Form", Form);
+
 app.mount("#app");
